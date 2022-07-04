dec7451df0bdb41bce0e9fa67b377e8385ea3bf9
misyu137 <53760119+misyu137@users.noreply.github.com>
2022-07-05 02:26:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,9 +12,9 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*passwall2\)/\1/' feeds.conf.default
+#sed -i 's/^#\(.*passwall2\)/\1/' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git PW2luci https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-echo "src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages" >> feeds.conf.default
-echo "src-git PWluci https://github.com/xiaorouji/openwrt-passwall.git;luci" >> feeds.conf.default
+echo "src-git kpackages https://github.com/kenzok8/openwrt-packages" >> feeds.conf.default
+echo "src-git small https://github.com/kenzok8/small" >> feeds.conf.default
